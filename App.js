02cba4bc5374ded3d6275545b97c099eca1896ba
Miguel Torres <miguel@alturasoluciones.com>
2018-06-02 15:47:20 -0500

    Flex Dimensions

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import FixedDimensionsBasics from './components/FixedDimensionsBasics'
+import FlexDimensionsBasics from './components/FlexDimensionsBasics'
 
 export default class App extends React.Component {
   render () {
     return (
-      <FixedDimensionsBasics/>
+      <FlexDimensionsBasics/>
     )
   }
 }
