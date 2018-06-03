10135670a821c8f18146ce66ef92d6aac4f1328c
Miguel Torres <miguel@alturasoluciones.com>
2018-06-02 22:26:42 -0500

    Networking

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import SectionListBasics from './components/SectionListBasics'
+import FetchExample from './components/FetchExample'
 
 export default class App extends React.Component {
   render () {
     return (
-      <SectionListBasics/>
+      <FetchExample/>
     )
   }
 }
